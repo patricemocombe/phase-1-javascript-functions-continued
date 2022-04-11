223d84d9bfbf486e7ac9e0de96013b7811f872e8
patrice mocombe <patricemocombe@gmail.com>
2022-04-11 11:51:48 -0400

    commit tree 4a652403c0262abea833c0f3fb521d3629614a98

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+};
+
+saturdayFun();
+
+function mondayWork(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`);
+};
+
+mondayWork();
+
+function wrapAdjective(adjective = '*') {
+    const inner = function(byDefault = "special"){
+        return `You are ${adjective}${byDefault}${adjective}!`;
+    }   
+    return inner;
+};
